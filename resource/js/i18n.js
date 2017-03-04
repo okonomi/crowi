02594a920e754710c87a27add4fccae7e59ac6fd
okonomi <oknm@okonomi.sakura.ne.jp>
2017-03-04 19:31:47 +0900

    Cleanup i18next setting

--- resource/js/i18n.js
+++ resource/js/i18n.js
@@ -1,33 +1,14 @@
 import i18n from 'i18next';
 import XHR from 'i18next-xhr-backend';
-// import Cache from 'i18next-localstorage-cache';
-
 
 i18n
   .use(XHR)
-  // .use(Cache)
   .init({
     fallbackLng: 'en',
 
-    // have a common namespace used around the full app
-    ns: ['translation'],
-    defaultNS: 'translation',
-
-    debug: true,
-
-    // cache: {
-    //   enabled: true
-    // },
-
     interpolation: {
-      escapeValue: false, // not needed for react!!
-      formatSeparator: ',',
-      format: function(value, format, lng) {
-        if (format === 'uppercase') return value.toUpperCase();
-        return value;
-      }
+      escapeValue: false
     }
   });
-
 
 export default i18n;
