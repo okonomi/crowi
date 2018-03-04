47c7ff299d8e90e1d4af23abb918830ace7e36f1
okonomi <oknm@okonomi.sakura.ne.jp>
2018-03-04 14:52:25 +0900

    css now on webpack pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,73 @@
 const path = require('path');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: {
-    app:          './resource/js/app.js',
-    crowi:        './resource/js/crowi.js',
-    presentation: './resource/js/crowi-presentation.js',
-    form:         './resource/js/crowi-form.js',
-    admin:        './resource/js/crowi-admin.js',
-    bundled:      './resource/js/bundled.js',
-    polyfill:     'babel-polyfill',
+module.exports = [
+  {
+    entry: {
+      app:          './resource/js/app.js',
+      crowi:        './resource/js/crowi.js',
+      presentation: './resource/js/crowi-presentation.js',
+      form:         './resource/js/crowi-form.js',
+      admin:        './resource/js/crowi-admin.js',
+      bundled:      './resource/js/bundled.js',
+      polyfill:     'babel-polyfill',
+    },
+    output: {
+      path: path.resolve(__dirname, 'public/js'),
+      filename: '[name].[hash].js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          exclude: /node_modules/,
+          use: [{
+            loader: 'babel-loader',
+          }]
+        }
+      ]
+    },
+    plugins: [
+      new ManifestPlugin(),
+    ],
   },
-  output: {
-    path: path.resolve(__dirname, 'public/js'),
-    filename: '[name].[hash].js'
+  {
+    entry: {
+      crowi: './resource/css/crowi.scss',
+    },
+    output: {
+      path: path.resolve(__dirname, 'public/css'),
+      filename: '[name].css'
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /.scss?$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader'
+              },
+              {
+                loader: 'sass-loader',
+              }
+            ]
+          })
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '../fonts/[name].[ext]',
+          }
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].css'),
+    ],
   },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new ManifestPlugin(),
-  ]
-};
+];
