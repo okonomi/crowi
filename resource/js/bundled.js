805e465ad4aa6173bb386c65667047d0e5ad746a
okonomi <oknm@okonomi.sakura.ne.jp>
2018-03-04 14:09:08 +0900

    fixup! Bundle traditional js

---
+++ resource/js/bundled.js
@@ -0,0 +1,8 @@
+const $ = require('jquery');
+window.$ = $;
+window.jQuery = $;
+
+require('bootstrap-sass');
+require('inline-attachment/src/inline-attachment.js');
+require('jquery.cookie');
+require('../thirdparty-js/jquery.selection.js');
